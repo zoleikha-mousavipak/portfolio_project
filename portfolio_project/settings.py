160a172fd05e4ba030d2ff41577333182cfa0ef5
msadour <sadour.mehdi@gmail.com>
2018-09-18 21:02:49 +0200

    db

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #     'NAME': 'portfolio_project',
@@ -89,7 +89,7 @@ DATABASES = {
     #     'HOST': '127.0.0.1',
     #     'PORT': '',
     # }
-    'default':dj_database_url.config(conn_max_age=600, ssl_require=True)
+    # 'default':dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
