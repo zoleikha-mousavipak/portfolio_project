a47ca8c957a63304027e282beea9abd045bdac7d
msadour <sadour.mehdi@gmail.com>
2018-09-19 03:00:31 +0200

    Update Django for database

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -92,6 +92,8 @@ DATABASES = {
     # 'default':dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
