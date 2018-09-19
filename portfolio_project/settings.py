d00915d9a1a17cb3b1319f06b5c4bfa5b1f77401
msadour <sadour.mehdi@gmail.com>
2018-09-19 15:25:26 +0200

    migrations

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,5 +139,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
 
 django_heroku.settings(locals())
--- test_db.py
+++ test_db.py
@@ -1,10 +1,23 @@
+"""
+Contain the functions for run application.
+"""
+
 import psycopg2
+from urllib.parse import urlparse
 
 
-# connexion = psycopg2.connect(dbname="portfolio",
-#                                      user="mehdi",
-#                                      host="127.0.0.1",
-#                                      password="azerty"
-#                                      )
+info_db = urlparse(
+            "postgres://hzajnzutyyndog:ab2cffd9f9a694f816728c6ca5133b21d6006156cf84a586be566e61a2e88cf7@ec2-54-247-98-162.eu-west-1.compute.amazonaws.com:5432/df6ko6h1vmn5gf")
 
-connexion = psycopg2.connect("dbname='portfolio_project' user='root' host='127.0.0.1' password=''" )
+username = info_db.username
+password = info_db.password
+database = info_db.path[1:]
+hostname = info_db.hostname
+connexion = psycopg2.connect(
+    database=database,
+    user=username,
+    password=password,
+    host=hostname
+)
+
+import pdb ; pdb.set_trace()
