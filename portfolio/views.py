6438c609c765af92e700ea3eec0cb91847f2d681
msadour <sadour.mehdi@gmail.com>
2019-02-06 21:32:30 +0100

    fix bug bdd

--- portfolio/views.py
+++ portfolio/views.py
@@ -13,12 +13,11 @@ from django.core.mail import send_mail
 
 
 data = {
-    # "experiences": [exp for exp in Experience.objects.all().order_by('date_begin').reverse()],
-    # "educations": [education for education in Education.objects.all().order_by('year').reverse()],
-    # "skills": [skill for skill in Skills.objects.all()],
-    # "projects": [project for project in PersonalProject.objects.all()],
-    # "form_contact": ContactForm()
-
+    "experiences": [exp for exp in Experience.objects.all().order_by('date_begin').reverse()],
+    "educations": [education for education in Education.objects.all().order_by('year').reverse()],
+    "skills": [skill for skill in Skills.objects.all()],
+    "projects": [project for project in PersonalProject.objects.all()],
+    "form_contact": ContactForm()
 }
 
 
@@ -93,91 +92,90 @@ def decode_data(string):
 
 
 def load_database(request):
-    pass
     #Delete all database records
-    # User.objects.all().delete()
-    # Task.objects.all().delete()
-    # Technology.objects.all().delete()
-    # Experience.objects.all().delete()
-    # Skills.objects.all().delete()
-    # Education.objects.all().delete()
-    # PersonalProject.objects.all().delete()
-    #
-    # current_directory = os.path.dirname(os.path.realpath(__file__))
-    # path_json_file = current_directory + "/load_data.json"
-    # my_dict = json.load(open(path_json_file))
-    # for classe, datas in my_dict.items():
-    #     if classe == 'PersonalProject':
-    #         for data in datas:
-    #             obj = PersonalProject(
-    #                 name = decode_data(data['name']),
-    #                 description = decode_data(data['description']),
-    #                 github = data['github'],
-    #                 link = data['link'],
-    #                 period = data['period'],
-    #                 num_project=data['num_project'])
-    #             obj.save()
-    #
-    #             for technologies_data in data['technologies']:
-    #                 obj_techno = Technology(name=decode_data(technologies_data["name"]), is_main=technologies_data["is_main"])
-    #                 obj_techno.save()
-    #                 obj.technologies.add(obj_techno)
-    #             obj.save()
-    #
-    #     elif classe == 'Experience':
-    #         for data in datas:
-    #             obj = Experience(
-    #                 company = data['company'],
-    #                 job = decode_data(data['job']),
-    #                 place = decode_data(data['place']),
-    #                 description_mission = decode_data(data['description_mission']),
-    #                 description_company = decode_data(data['description_company']),
-    #                 type_experience = data['type_experience'],
-    #                 date_begin = data['date_begin'],
-    #                 date_end = data['date_end']
-    #             )
-    #             obj.save()
-    #
-    #             for tasks_data in data['tasks']:
-    #                 obj_task = Task(name=decode_data(tasks_data["name"]))
-    #                 obj_task.save()
-    #                 obj.tasks.add(obj_task)
-    #
-    #             for technologies_data in data['technologies']:
-    #                 obj_techno = Technology(name=decode_data(technologies_data["name"]), is_main=technologies_data["is_main"])
-    #                 obj_techno.save()
-    #                 obj.technologies.add(obj_techno)
-    #             obj.save()
-    #
-    #     elif classe == 'User':
-    #         for data in datas:
-    #             user = User.objects.create_user(
-    #                 username = data["username"],
-    #                 first_name = data["first_name"],
-    #                 last_name = data["last_name"],
-    #                 email = data["email"],
-    #                 password = data["password"],
-    #                 is_staff = data["is_staff"]
-    #             )
-    #             user.save()
-    #     elif classe == 'Skills':
-    #         for data in datas:
-    #             obj = Skills(
-    #                 name = decode_data(data["name"]),
-    #                 level = data["level"]
-    #             )
-    #             obj.save()
-    #
-    #     elif classe == 'Education':
-    #         for data in datas:
-    #             obj = Education(
-    #                 name = decode_data(data["name"]),
-    #                 option = decode_data(data["option"]),
-    #                 school = decode_data(data["school"]),
-    #                 year = data["year"],
-    #                 place = decode_data(data["place"]),
-    #                 description = decode_data(data["description"])
-    #             )
-    #             obj.save()
-    # return home(request)
-    # return render(request, 'portefolio.html')
+    User.objects.all().delete()
+    Task.objects.all().delete()
+    Technology.objects.all().delete()
+    Experience.objects.all().delete()
+    Skills.objects.all().delete()
+    Education.objects.all().delete()
+    PersonalProject.objects.all().delete()
+
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    path_json_file = current_directory + "/load_data.json"
+    my_dict = json.load(open(path_json_file))
+    for classe, datas in my_dict.items():
+        if classe == 'PersonalProject':
+            for data in datas:
+                obj = PersonalProject(
+                    name = decode_data(data['name']),
+                    description = decode_data(data['description']),
+                    github = data['github'],
+                    link = data['link'],
+                    period = data['period'],
+                    num_project=data['num_project'])
+                obj.save()
+
+                for technologies_data in data['technologies']:
+                    obj_techno = Technology(name=decode_data(technologies_data["name"]), is_main=technologies_data["is_main"])
+                    obj_techno.save()
+                    obj.technologies.add(obj_techno)
+                obj.save()
+
+        elif classe == 'Experience':
+            for data in datas:
+                obj = Experience(
+                    company = data['company'],
+                    job = decode_data(data['job']),
+                    place = decode_data(data['place']),
+                    description_mission = decode_data(data['description_mission']),
+                    description_company = decode_data(data['description_company']),
+                    type_experience = data['type_experience'],
+                    date_begin = data['date_begin'],
+                    date_end = data['date_end']
+                )
+                obj.save()
+
+                for tasks_data in data['tasks']:
+                    obj_task = Task(name=decode_data(tasks_data["name"]))
+                    obj_task.save()
+                    obj.tasks.add(obj_task)
+
+                for technologies_data in data['technologies']:
+                    obj_techno = Technology(name=decode_data(technologies_data["name"]), is_main=technologies_data["is_main"])
+                    obj_techno.save()
+                    obj.technologies.add(obj_techno)
+                obj.save()
+
+        elif classe == 'User':
+            for data in datas:
+                user = User.objects.create_user(
+                    username = data["username"],
+                    first_name = data["first_name"],
+                    last_name = data["last_name"],
+                    email = data["email"],
+                    password = data["password"],
+                    is_staff = data["is_staff"]
+                )
+                user.save()
+        elif classe == 'Skills':
+            for data in datas:
+                obj = Skills(
+                    name = decode_data(data["name"]),
+                    level = data["level"]
+                )
+                obj.save()
+
+        elif classe == 'Education':
+            for data in datas:
+                obj = Education(
+                    name = decode_data(data["name"]),
+                    option = decode_data(data["option"]),
+                    school = decode_data(data["school"]),
+                    year = data["year"],
+                    place = decode_data(data["place"]),
+                    description = decode_data(data["description"])
+                )
+                obj.save()
+    return home(request)
+    return render(request, 'portefolio.html')
